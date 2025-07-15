02ff0d0805c47e5a3c4de0670a641828ae05dcd5
SSU <matrixmatrixmatrix228@gmail.com>
2025-07-15 11:05:14 +0300

    Add maven-publish for TerminatorHookKotlin

--- TerminatorHookKotlin/build.gradle.kts
+++ TerminatorHookKotlin/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.android)
+    id("maven-publish")
 }
 
 android {
@@ -29,10 +30,27 @@ android {
     kotlinOptions {
         jvmTarget = "11"
     }
+
+    publishing {
+        singleVariant("release")
+    }
 }
 
 dependencies {
 
     implementation ("org.jetbrains.kotlin:kotlin-reflect:1.9.0")
     implementation(project(":Terminator"))
+}
+
+afterEvaluate {
+    publishing {
+        publications {
+            register<MavenPublication>("release") {
+                from(components["release"])
+                groupId = "com.github.Matrix-Username"
+                artifactId = "TerminatorHookKotlin"
+                version = "1.0.0"
+            }
+        }
+    }
 }
